9f0bc1c015003b27968e329bb8fae4f3dc09c0a8
ReasonAmu <luongvantiem@gmail.com>
2017-02-16 04:31:07 +0700

    add

--- TalkTV/BaseVC.swift
+++ TalkTV/BaseVC.swift
@@ -39,6 +39,7 @@ class BaseVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.automaticallyAdjustsScrollViewInsets = false
         setupUI()
         // Do any additional setup after loading the view.
     }
